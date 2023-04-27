811b154881732112456ccf96c9e3558e780843e0
m00n-die <95586757+m00n-die@users.noreply.github.com>
2023-04-27 00:16:09 +0000

    Update sort.h

--- sort.h
+++ sort.h
@@ -34,47 +34,9 @@ typedef struct listint_s
 } listint_t;
 
 /* Printing helper functions */
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list)
-{
-    int i;
-
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
-}
 
 /* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
